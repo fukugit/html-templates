fa4867dc792aa76af993685f24f5d6d58744eac5
fukugit <fukuma1234@gmail.com>
2023-09-07 18:36:21 +0900

    Add templat of 1_normal_image_gallery

---
+++ 1_normal_image_gallery/module.js
@@ -0,0 +1,10 @@
+import { annotate } from 'https://unpkg.com/rough-notation?module';
+
+const e1 = document.querySelector('.item');
+annotate(e1, { type: 'box' }).show();
+
+// const e2 = document.querySelector('#element2');
+// const annotation2 = annotate(e2, { type: 'box' });
+
+annotation1.show();
+// annotation2.show();
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # html-templates
+This project offers various types of HTML templates. Feel free to use any of these templates as a foundation for your web system.  
+
+## Templates
+Below are HTML templates and a sample webpage using these templates.  
